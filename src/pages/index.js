22f3d7ddd6aee340701b6d2883d40e514610a261
kondratyev <kondratyev.work@gmail.com>
2018-10-02 10:38:17 +0300

    initialize

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => (
+  <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby!</h1>
+    <p>What a world.</p>
+	<Link to="/about/">About</Link>
+  </div>
+)
