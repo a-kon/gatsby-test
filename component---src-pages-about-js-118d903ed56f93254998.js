0f2406f807d4e1ffae2eb55335a04dfdb8c326bf
kondratyev <kondratyev.work@gmail.com>
2018-10-02 10:43:01 +0300

    Updates

---
+++ component---src-pages-about-js-118d903ed56f93254998.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{137:function(e,t,n){"use strict";n.r(t);var a=n(0),c=n.n(a);t.default=function(){return c.a.createElement("div",{style:{color:"teal"}},c.a.createElement("h1",null,"About Gatsby"),c.a.createElement("p",null,"Such wow. Very React."))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-118d903ed56f93254998.js.map
